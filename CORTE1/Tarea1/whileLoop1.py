bc13618925496c4da99e075aa7f0a822491556fb
daniel24294 <gonzalezedisondaniel24@gmail.com>
2026-09-23 13:43:18 -0500

    Create whileLoop1.py

---
+++ CORTE1/Tarea1/whileLoop1.py
@@ -0,0 +1,5 @@
+for i in range(1,6):  -> Recorre los valores de 1 a 5.
+    while i <= 4:      -> Repite mientras i sea menor o igual a 4.
+        i += 1         -> Incrementa i en 1.
+        print(i)       -> Imprime el valor actual de i.
+    break              -> Termina el ciclo for después de la primera iteración.
